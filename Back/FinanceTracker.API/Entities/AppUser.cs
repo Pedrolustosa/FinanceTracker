9805860cfda803fec5eb2cc92645725dfb5948bc
Pedrolustosa <pedroeternalss@gmail.com>
2024-08-06 19:56:46 -0300

    feat: Extending the user entity

--- Back/FinanceTracker.API/Entities/AppUser.cs
+++ Back/FinanceTracker.API/Entities/AppUser.cs
@@ -4,6 +4,15 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+    public DateOnly DateOfBirth { get; set; }
+    public required string Knows { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string? Introdution { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public List<Photo> Photos { get; set; } = [];
 }
---
+++ Back/FinanceTracker.API/Entities/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceTracker.API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string? PublicId { get; set; }
+}
