289e532c2e5c47d300e05aaeb63d069ba3c15dd9
Pedrolustosa <pedroeternalss@gmail.com>
2024-09-25 20:39:30 -0300

    feat: Identity and Role Management - Part III
    - Added admin component to manage users and roles
    - Implemented admin guard to protect admin routes

--- Front/FinanceTracker.Client/src/app/_services/account.service.ts
+++ Front/FinanceTracker.Client/src/app/_services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -11,6 +11,16 @@ export class AccountService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
+  roles = computed(() => {
+    const user = this.currentUser();
+    if(user && user.token)
+    {
+      const role = JSON.parse(atob(user.token.split('.')[1])).role
+      return Array.isArray(role) ? role : [role];
+    }
+
+    return [];
+  })
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
